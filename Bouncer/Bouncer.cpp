6e8cf361e6fb9b2eb17a115691be714d584f2e6e
Rushil <rushil.kekre@gmail.com>
2017-11-07 03:30:36 -0600

    mouse and scroll callback functions created render_sphere function created ball shader created and initialized display ball on screen

--- Bouncer/Bouncer.cpp
+++ Bouncer/Bouncer.cpp
@@ -1,36 +1,62 @@
 /*
-Rushil Kekre
-Bouncer.cpp
-Bouncing ball simulation
-C++, OpenGL, GLSL
-11/05/2017
+    Rushil Kekre
+    Bouncer.cpp
+    Bouncing ball simulation
+    C++, OpenGL, GLSL
+    11/05/2017
 */
 
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
-
-// OpenGL headers
+//  OpenGL headers
 #include<glad/glad.h>
 #include<glfw/glfw3.h>
 
+//  GLM
+#include<glm/glm.hpp>
 
-// C++ headers
+//  C++ headers
 #include<iostream>
 
+//  Custon headers
+#include "Camera.h"
+#include "Shader.h"
 
-// Callback function definitions
+//  Callback function definitions
+void process_input(GLFWwindow* window); 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void process_input(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double x_pos, double y_pos);
+void scroll_callback(GLFWwindow* window, double x_offset, double y_offset);
+
+//  Shape functions
+void render_sphere();
+
+//  Global variables
+
+//  Screen
+const unsigned int SCREEN_WIDTH = 1280;
+const unsigned int SCREEN_HEIGHT = 720;
+
+//  Time
+float delta_time = 0.0;
+float last_frame = 0.0;
+
+//  Camera
+Camera camera(glm::vec3(0.0, 0.0, 5.0));
+float last_x = SCREEN_WIDTH / 2.0;
+float last_y = SCREEN_HEIGHT / 2.0;
+bool first_mouse = true;
+
+//  Shaders
+Shader ball;
 
 int main() {
 
-    // GLFW: Initialization
+    //  GLFW: Initialization
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
        
-    // GLFW: Window creation
+    //  GLFW: Window creation
     GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH,SCREEN_HEIGHT,"PBM",NULL,NULL);
     if (window==NULL) {
         std::cout << "Failed to create GLFW Window" << std::endl;
@@ -39,39 +65,194 @@ int main() {
     }
     glfwMakeContextCurrent(window);
 
-    // Set callback functions
+    //  Set callback functions
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
 
-    // GLAD: Initialization
+    //  GLAD: Initialization
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // RENDER LOOP
+    //  Load Shader
+    ball.loadShader("ball.vert", "ball.frag");
+
+    //  RENDER LOOP
     while (!glfwWindowShouldClose(window)) {
 
-        // Process input
+        //  Process input
         process_input(window);
 
         glClearColor(0.2, 0.2, 0.2,1.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Swap buffers and poll IO events
+        ball.Use();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+        ball.setMat4("projection", projection);
+        glm::mat4 view = camera.GetViewMatrix();
+        ball.setMat4("view", view);
+        glm::mat4 model;
+        model = glm::scale(model, glm::vec3(1.0f));
+        ball.setMat4("model", model);
+        render_sphere();
+
+        //  Swap buffers and poll IO events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // terminate
+    //  terminate
     glfwTerminate();
     return 0;
 }
 
+//
+//  Whenever the window size is changed (automatically by OS, or manually by user) this function is called
+//
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
+//
+//  Processes all input
+//  When input keys are pressed or camera is moved, this function reacts acccordingly
+//
 void process_input(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, delta_time);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, delta_time);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, delta_time);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, delta_time);
+}
+
+//
+//  Whenever the mouse moves, this function is called  
+//
+void mouse_callback(GLFWwindow* window, double x_pos, double y_pos) {
+    if (first_mouse) {
+        last_x = x_pos;
+        last_y = y_pos;
+        first_mouse = false;
+    }
+
+    float x_offset = x_pos - last_x;
+    float y_offset = last_y - y_pos;
+
+    last_x = x_pos;
+    last_y = y_pos;
+
+    camera.ProcessMouseMovement(x_offset, y_offset);
+}
+
+//
+//  Whenever mouse scroll wheel is used, this function is called
+//
+void scroll_callback(GLFWwindow* window, double x_offset, double y_offset) {
+    camera.ProcessMouseScroll(y_offset);
+}
+
+//
+//  Renders a sphere
+//
+GLuint sphereVAO = 0;
+GLuint indexCount;
+void render_sphere()
+{
+    if (sphereVAO == 0)
+    {
+        glGenVertexArrays(1, &sphereVAO);
+
+        GLuint vbo, ebo;
+        glGenBuffers(1, &vbo);
+        glGenBuffers(1, &ebo);
+
+        std::vector<glm::vec3> positions;
+        std::vector<glm::vec2> uv;
+        std::vector<glm::vec3> normals;
+        std::vector<GLuint> indices;
+
+        const GLuint X_SEGMENTS = 64;
+        const GLuint Y_SEGMENTS = 64;
+        const float PI = 3.14159265359;
+        for (GLuint y = 0; y <= Y_SEGMENTS; ++y)
+        {
+            for (GLuint x = 0; x <= X_SEGMENTS; ++x)
+            {
+                float xSegment = (float)x / (float)X_SEGMENTS;
+                float ySegment = (float)y / (float)Y_SEGMENTS;
+                float xPos = std::cos(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+                float yPos = std::cos(ySegment * PI);
+                float zPos = std::sin(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+
+                positions.push_back(glm::vec3(xPos, yPos, zPos));
+                uv.push_back(glm::vec2(xSegment, ySegment));
+                normals.push_back(glm::vec3(xPos, yPos, zPos));
+            }
+        }
+
+        bool oddRow = false;
+        for (int y = 0; y < Y_SEGMENTS; ++y)
+        {
+            if (!oddRow) // even rows: y == 0, y == 2; and so on
+            {
+                for (int x = 0; x <= X_SEGMENTS; ++x)
+                {
+                    indices.push_back(y       * (X_SEGMENTS + 1) + x);
+                    indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                }
+            }
+            else
+            {
+                for (int x = X_SEGMENTS; x >= 0; --x)
+                {
+                    indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                    indices.push_back(y       * (X_SEGMENTS + 1) + x);
+                }
+            }
+            oddRow = !oddRow;
+        }
+        indexCount = indices.size();
+
+        std::vector<float> data;
+        for (int i = 0; i < positions.size(); ++i)
+        {
+            data.push_back(positions[i].x);
+            data.push_back(positions[i].y);
+            data.push_back(positions[i].z);
+            if (uv.size() > 0)
+            {
+                data.push_back(uv[i].x);
+                data.push_back(uv[i].y);
+            }
+            if (normals.size() > 0)
+            {
+                data.push_back(normals[i].x);
+                data.push_back(normals[i].y);
+                data.push_back(normals[i].z);
+            }
+        }
+        glBindVertexArray(sphereVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
+        float stride = (3 + 2 + 3) * sizeof(float);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, stride, (void*)(5 * sizeof(float)));
+    }
+
+    glBindVertexArray(sphereVAO);
+    glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
 }
