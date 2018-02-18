2cb86d02368fddec588f5cc945277d148d30af7f
Rushil <rushil.kekre@gmail.com>
2018-02-17 19:09:00 -0800

    Collision response implemented

--- Bouncer/Simulation.cpp
+++ Bouncer/Simulation.cpp
@@ -1,5 +1,12 @@
 #include "Simulation.h"
 
+
+//  Global Variables
+std::vector<glm::vec3> planePositions(6);   //  DS to store plane positions and normals used for collision detection
+std::vector<glm::vec3> planeNormals(6);
+std::vector<bool> collisionPlane(6, false);
+
+
 void StartSimulation(glm::vec3 ballPosition) {
     //  initial conditions
     std::cout << "simulation started" << std::endl;
@@ -23,16 +30,13 @@ Information stored in the following manner
 [4] ->  +Z  ->  FRONT
 [5] ->  -Z  ->  BACK
 */
-//  Global Variables
-std::vector<glm::vec3> planePositions(6);   //  DS to store plane positions and normals used for collision detection
-std::vector<glm::vec3> planeNormals(6);
 void ConfigurePlanes() {
-    planePositions[0] = glm::vec3(5.0f, 0.0, 0.0);
-    planePositions[1] = glm::vec3(-5.0f, 0.0, 0.0);
-    planePositions[2] = glm::vec3(0.0, 5.0f, 0.0);
-    planePositions[3] = glm::vec3(0.0, -5.0f, 0.0);
-    planePositions[4] = glm::vec3(0.0, 0.0, 5.0f);
-    planePositions[5] = glm::vec3(0.0, 0.0, -5.0f);
+    planePositions[0] = glm::vec3(15.0f, 0.0, 0.0);
+    planePositions[1] = glm::vec3(-15.0f, 0.0, 0.0);
+    planePositions[2] = glm::vec3(0.0, 15.0f, 0.0);
+    planePositions[3] = glm::vec3(0.0, -15.0f, 0.0);
+    planePositions[4] = glm::vec3(0.0, 0.0, 15.0f);
+    planePositions[5] = glm::vec3(0.0, 0.0, -15.0f);
 
     glm::vec3 negative(-1.0);
     planeNormals[0] = glm::normalize(planePositions[0] * negative) ;
@@ -55,36 +59,33 @@ void ConfigurePlanes() {
     h -> timestep
 */
 glm::vec3 UpdatePosition(glm::vec3 ballPosition) {
-    static glm::vec3 velocity = glm::vec3(0, 0.8, 0);      //  starting velocity
-    const glm::vec3 gravity = glm::vec3(0.0, -9.8, 0.0);    //  constant gravity
-    static glm::vec3 position = ballPosition;               //  starting position
-    static float h = 0.01;                                  //  timestep
-    const float mass = 1.0;                                 //  mass of ball
-    float airResistanceConstant = 0.5;                      //  constant for air resistance
-    glm::vec3 windVelocity = glm::vec3(0, 0, 0);            //  wind velocity
+    float h = 0.01f;                                            //  timestep
+    float f = 1.0f;                                             //  fraction
+    glm::vec3 velocity = glm::vec3(5.0f, 10.8f, 0.0f);          //  starting velocity
+    const glm::vec3 gravity = glm::vec3(0.0f, -9.8f, 0.0f);     //  constant gravity
+    const float mass = 1.0f;                                    //  mass of ball
+    float airResistanceConstant = 0.5f;                         //  constant for air resistance
+    glm::vec3 windVelocity = glm::vec3(-4.0f, 0.0f, 0.0f);      //  wind velocity
 
     //  Calculating acceleration taking into account gravity and air resistance
     glm::vec3 acceleration = gravity + (airResistanceConstant / mass)*(windVelocity - velocity);
+    //  Euler simulation
+    glm::vec3 newVelocity = velocity + acceleration*h;
+    glm::vec3 newPosition = ballPosition + h*((newVelocity + velocity) / 2.0f);
     
     //  Check for collisions
-    if (CollisionCheck(position)) {
+    if (CollisionCheck(ballPosition)) {
         //std::cout << "Collision occured" << std::endl;
-        return glm::vec3(0.0);
+        ballPosition = ballPosition;
     }
     else {
         //std::cout << "Finding new positions" << std::endl;
-
-        //  Euler simulation
-        glm::vec3 newVelocity = velocity + acceleration*h;
-        glm::vec3 newPosition = position + h*((newVelocity + velocity) / 2.0f);
-
-
         //  Updating velocity and position for next frame
         velocity = newVelocity;
-        position = newPosition;
-
-        return newPosition * 0.1f;
+        ballPosition = newPosition;
     }
+
+    return ballPosition;
     
     //if(newPosition.y>0)
     //  std::cout << newVelocity.x <<" "<< newVelocity.y <<" " << newVelocity.z <<"\t" << newPosition.x << " " << newPosition.y << " " <<newPosition.z << std::endl;
@@ -96,17 +97,66 @@ glm::vec3 UpdatePosition(glm::vec3 ballPosition) {
 */
 bool CollisionCheck(glm::vec3 position) {
     //std::cout << position.x << " " << position.y << " " << position.z << std::endl;
-    float radius = 1.0f;
-    glm::vec3 difference = position - planePositions[3];
-    //std::cout << "Diff: " << difference.x <<" "<<difference.y<<" "<<difference.z << std::endl;
-    float dotProd = glm::dot(difference, planeNormals[3]);
-    //std::cout << dotProd << std::endl;
-    float distance = dotProd - radius;
-    //std::cout << distance << std::endl;
+    float radius = 1.25f;
+    
+    for (int i = 0; i < planePositions.size(); i++) {
+        glm::vec3 difference = position - planePositions[i];
+        //std::cout << "Diff: " << difference.x <<" "<<difference.y<<" "<<difference.z << std::endl;
+        float dotProd = glm::dot(difference, planeNormals[i]);
+        //std::cout << dotProd << std::endl;
+        float distance = dotProd - radius;
+        //std::cout << distance << std::endl;
 
-    if (distance > 0)
-        return false;
-    else
-        return true;
+        if (distance < 0) {
+            collisionPlane[i] = true;
+            return true;
+        }
+    }
+    
+    //  if not returned from for loop, return false since no collision detected
+    return false;
 }
 
+float FindDistance(glm::vec3 position) {
+    float distance = FLT_MAX;
+    float radius = 1.25f;
+
+    for (int i = 0; i < planePositions.size(); i++) {
+        glm::vec3 difference = position - planePositions[i];
+        //std::cout << "Diff: " << difference.x <<" "<<difference.y<<" "<<difference.z << std::endl;
+        float dotProd = glm::dot(difference, planeNormals[i]);
+        //std::cout << dotProd << std::endl;
+        float newDistance = dotProd - radius;
+        //std::cout << distance << std::endl;
+
+        distance = std::min(distance, newDistance);
+    }
+
+    return distance;
+}
+
+glm::vec3 CollisionResponse(glm::vec3 velocity) {
+    int planeIndex = 0;
+    float coe = 1.0f;       //  Coefficient of Elasticity
+    float cof = 0.1f;       //  Coefficient of Friction
+    
+    //  Find the right plane where collision takes place
+    for (int i = 0; i < collisionPlane.size(); i++) {
+        if (collisionPlane[i])
+            planeIndex = i;
+    }
+    glm::vec3 normal = planeNormals[planeIndex];
+
+    //  Calculate normal and tangential velocity
+    glm::vec3 normalVelocity = dot(velocity, normal) * normal;
+    glm::vec3 tangentVelocity = velocity - normalVelocity;
+
+    //  Calculate elastic and frictional veclocities
+    glm::vec3 elasticVelocity = -1.0f * coe * normalVelocity;
+    glm::vec3 frictionVelocity = (1.0f - cof) * tangentVelocity;
+
+    //  Calculate new velocity
+    glm::vec3 newVelocity = elasticVelocity + frictionVelocity;
+
+    return newVelocity;
+}
--- Bouncer/Simulation.h
+++ Bouncer/Simulation.h
@@ -6,6 +6,8 @@
 //  Headers
 #include <iostream>
 #include <vector>
+#include <float.h>
+#include <algorithm>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
@@ -14,5 +16,7 @@ void StartSimulation(glm::vec3 ballPosition);
 void ConfigurePlanes();
 glm::vec3 UpdatePosition(glm::vec3 ballPosition);
 bool CollisionCheck(glm::vec3 position);
+float FindDistance(glm::vec3 position);
+glm::vec3 CollisionResponse(glm::vec3 velocity);
 
 #endif // !SIMULATION_H
